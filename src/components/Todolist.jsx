54015025dceee59c6f6738bb4670111f0c42cb36
danish <danishofcodes@gmail.com>
2024-03-19 23:28:53 +0530

    removed the 'remove button' to make room for todo, in its place  added 'clear marked' button that removes each  completed items ...

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -2,12 +2,13 @@ export default function Todolist({ item, handleChange, handleClick }) {
   return (
     // striking off the checked items conditionally
     <li id={item.id} >
-      <div className="todo-container" >
-        <div className="todo" style={{ textDecoration: item.complete ? 'line-through' : 'none' }}>
-          <input type="checkbox" onChange={handleChange} checked={item.complete} />
-          <label htmlFor={item.id}>{item.todo}</label>
+      <div className="flex justify-between my-2" >
+        <div className="checkboxes flex">
+          <label htmlFor={item.id} className="text-left" style={{ textDecoration: item.complete ? 'line-through' : 'none' }}> 
+          <input className="accent-emerald-300/25" type="checkbox" onChange={handleChange} checked={item.complete} />
+          <span className="ms-1 text-teal-900">{item.todo}</span></label>
         </div>
-        <button className="delete-todo" onClick={handleClick} type="button">remove</button>
+        {/* <button className="removeBtn bg-teal-500  text-sm px-2 rounded-[20px] hover:bg-[#ef4444] text-[#fff]" onClick={handleClick} type="button">Remove</button> */}
       </div>
     </li>
   );
